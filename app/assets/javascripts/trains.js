e9ce1a7d11f48fc8f40ffaf2f8b7e0858edb2c54
Geoff Crittenden <geoffcritt@hotmail.com>
2013-10-19 13:29:02 -0500

    working it

--- app/assets/javascripts/trains.js
+++ app/assets/javascripts/trains.js
@@ -1,14 +1,11 @@
 function trains(map) {
   $.get('/trains', function(response){
-    console.log(response[2]);
-    var redTrains    = response[0].getElementsByTagName("train");
-    var greenTrains  = response[1].getElementsByTagName("train");
-    var blueTrains   = response[2].getElementsByTagName("train");
-    var brownTrains  = response[3].getElementsByTagName("train");
-    var pinkTrains   = response[4].getElementsByTagName("train");
-    var orangeTrains = response[5].getElementsByTagName("train");
-    var purpleTrains = response[6].getElementsByTagName("train");
-    var yellowTrains = response[7].getElementsByTagName("train");
+    console.log(response[0]);
+    var xmlTrains = $.parseXML( response[0] ),
+      $xml = $( xmlTrains ),
+      $train = $xml.find( "train" );
+    console.log($train);
+    // var trains = response[0].getElementsByTagName("train");
     // console.log(result);
     // $.each(response, function(index, value){
 
