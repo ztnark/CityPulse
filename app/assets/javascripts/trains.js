71b0644188d0106f7797cbe20a4a736bd3f40c3e
Geoff Crittenden <geoffcritt@hotmail.com>
2013-10-19 13:06:33 -0500

    trains data

--- app/assets/javascripts/trains.js
+++ app/assets/javascripts/trains.js
@@ -1,7 +1,14 @@
 function trains(map) {
   $.get('/trains', function(response){
     console.log(response[2]);
-    var result = response[0].getElementsByTagName("train")[0];
+    var redTrains    = response[0].getElementsByTagName("train");
+    var greenTrains  = response[1].getElementsByTagName("train");
+    var blueTrains   = response[2].getElementsByTagName("train");
+    var brownTrains  = response[3].getElementsByTagName("train");
+    var pinkTrains   = response[4].getElementsByTagName("train");
+    var orangeTrains = response[5].getElementsByTagName("train");
+    var purpleTrains = response[6].getElementsByTagName("train");
+    var yellowTrains = response[7].getElementsByTagName("train");
     // console.log(result);
     // $.each(response, function(index, value){
 
