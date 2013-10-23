54b689b70367953b10b145ab60b415b5116b8943
Geoff Crittenden <geoffcritt@hotmail.com>
2013-10-23 11:00:08 -0500

    cleaned up code in instagram.js function setMarker

--- app/assets/javascripts/instagram.js
+++ app/assets/javascripts/instagram.js
@@ -1,36 +1,26 @@
 function setMarker(lat, lon, map, val) {
-  // console.log(lat, lon, map);
   var latLng = new google.maps.LatLng(lat, lon);
-
   var marker = new google.maps.Marker({
     position: latLng,
     map: map,
     icon: 'http://www.charlestonstems.com/wp-content/themes/stems/images/instagram_icon_22x22.png',
     animation: google.maps.Animation.BOUNCE,
   });
-   setTimeout(function(){ marker.setAnimation(null); }, 750);
+  setTimeout(function(){ marker.setAnimation(null); }, 750);
   var infoWindowOptions = {
     content:  val
   };
   var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
   google.maps.event.addListener(marker,'mouseover', function(e){
-    // console.log(e);
-    // $('.gm-style-iw').close();
     infoWindow.open(map,marker);
   });
   google.maps.event.addListener(marker,'mouseout', function(e){
-    // console.log(e);
-    // $('.gm-style-iw').close();
     infoWindow.close();
   });
   google.maps.event.addListener(marker,'click', function(e){
-    // $('.gm-style-iw').close();
     var link = val.match(/(href=)(.+)(\starget)/)[2]
-    console.log(link);
     window.open(link);
   });
-
   setTimeout(function(){marker.setMap(null)},240000);
-
 };
 
