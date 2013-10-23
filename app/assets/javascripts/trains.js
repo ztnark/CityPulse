37fff0620c0aa55c9dc697ae5cc6061a6bef3ee1
Geoff Crittenden <geoffcritt@hotmail.com>
2013-10-23 11:45:35 -0500

    map marker fadeOut does not work

--- app/assets/javascripts/trains.js
+++ app/assets/javascripts/trains.js
@@ -28,5 +28,6 @@ function trainMarker(lat, lon, map, line, val) {
 
 function removeMarker(marker){
   setTimeout(function(){marker.setMap(null)},14750);
+  // setTimeout(function(){marker.fadeOut()});
 }
 
