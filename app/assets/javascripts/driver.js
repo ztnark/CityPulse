0369ceddd8a8cdc17da591282706e690428207e7
Geoff Crittenden <geoffcritt@hotmail.com>
2013-11-07 10:34:03 -0600

    increased instagram size in infoWindow from 100 to 200

--- app/assets/javascripts/driver.js
+++ app/assets/javascripts/driver.js
@@ -196,8 +196,9 @@ function stadiumThrob(stadium){
   var colcounter = 1;
   // var idcounter = 1;
   instagram.bind("events.instagram_success", function(message){
+    var url = message.url.replace(/(width=)(\d{3})(\sheight=)(\d{3})/,"$1200$3200");
     var $that = $("#instafeed #column" + colcounter).prepend("<div id='instaitem'>" + "<div class='instagram'>" + message.url + "</div><div class='lat'>" + message.latitude + "</div>" + "<div class='lon'>"+ message.longitude +"</div></div>");
-    setMarker(message.latitude, message.longitude, map, message.url);
+    setMarker(message.latitude, message.longitude, map, url);
     // setTimeout(function(){
       // var id_remove = '#' + (idcounter - 1)
        // $(id_remove).remove()
