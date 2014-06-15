9458d977801073ea1f0ee9109bf26269deac1994
hdicksonjr <hcdicksonjr@gmail.com>
2014-06-15 15:15:41 -0400

    clean deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,29 +1,16 @@
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
-# require 'mina/rvm'    # for rvm support. (http://rvm.io)
+require 'mina/rbenv'  
 
-# Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
-
-set :domain, '107.170.178.218'
+set :domain, '107.170.178.218' 
 set :deploy_to, '/var/www/citypulse.io/'
 set :repository, 'git@github.com:ztnark/CityPulse.git'
-set :branch, 'new-deploy'
+set :branch, ENV['branch'] || 'master'
 
-# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
-# They will be linked in the 'deploy:link_shared_paths' step.
 
-# Optional settings:
 set :user, 'root'    # Username in the server to SSH to.
-#   set :port, '30000'     # SSH port number.
 
-# This task is the environment that is loaded for most commands, such as
-# `mina deploy` or `mina rake`.
 task :environment do
   invoke :'rbenv:load'
 end
@@ -32,6 +19,5 @@ desc "Deploys the current version to the server."
 task :deploy => :environment do
   deploy do
     invoke :'git:clone'
-
   end
 end
